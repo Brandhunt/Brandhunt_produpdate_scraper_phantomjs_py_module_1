2ac6e2963307a7f1bef19fd491df0aedaa91e1bd
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-21 15:34:35 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,20 +19,13 @@ jsonpy = json.loads(r.content)
 filecont = jsonpy[0]['file_cont']
 filecont = json.loads(filecont)
 
-#with open('prod_update_copy.py','w') as newfile:
-#    for cont in filecont:
-#        newfile.write(cont)
-
 spec = importlib.util.spec_from_loader('helper', loader=None)
 helper = importlib.util.module_from_spec(spec)
 newcont = ''
-count = 1
+
+del filecont[-1:-3]
+
 for cont in filecont:
-    if count < 41 or count > 55:
-        newcont = newcont + cont
-    count = count + 1
+    newcont = newcont + cont
 exec(newcont, helper.__dict__)
 exec('mainfunc()', helper.__dict__)
-
-#print(type(helper)) # prints "<class 'module'>"
-#helper.a # prints "5"
