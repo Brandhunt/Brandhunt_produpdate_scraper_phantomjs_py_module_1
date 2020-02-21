a1dc28f6cdfc90dc0b18565d498203a4481386f1
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-21 05:57:41 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ helper = importlib.util.module_from_spec(spec)
 newcont = ''
 count = 1
 for cont in filecont:
-    if count < 41 || count > 55:
+    if count < 41 or count > 55:
         newcont = newcont + cont
     count = count + 1
 #    exec(cont, helper.__dict__)
