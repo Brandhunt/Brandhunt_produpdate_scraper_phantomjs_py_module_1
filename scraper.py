78a0db2caa93a3e3f1c4df371534938ed427751a
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-21 05:48:53 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,10 +19,14 @@ jsonpy = json.loads(r.content)
 filecont = jsonpy[0]['file_cont']
 filecont = json.loads(filecont)
 
-spec = importlib.util.spec_from_loader('helper', loader=None)
-helper = importlib.util.module_from_spec(spec)
-for cont in filecont:
-    exec(cont, helper.__dict__)
+with open('prod_update_copy.py','w') as newfile:
+    for cont in filecont:
+        newfile.write(cont)
 
-print(type(helper)) # prints "<class 'module'>"
+#spec = importlib.util.spec_from_loader('helper', loader=None)
+#helper = importlib.util.module_from_spec(spec)
+#for cont in filecont:
+#    exec(cont, helper.__dict__)
+
+#print(type(helper)) # prints "<class 'module'>"
 #helper.a # prints "5"
